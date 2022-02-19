00f6103f7254b1e3d62ac255d532f34efb22c779
xianzhu.liu <785666476@qq.com>
2022-02-19 20:54:13 +0800

    first_commit

---
+++ first_commit/git_commit.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# Author :xianzhu.liu
+# @Time    : 2022/2/19 20:51
+# @Site    : 
+# @File    : git_commit.py
+# @Software: PyCharm
+name = 'git@github.com:adadad12/first.git'
